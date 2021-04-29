243f641af0614e823d433feacd1d39eb93beefa4
Rick Mann <rmann@latencyzero.com>
2021-04-29 01:15:15 -0700

    Initial Commit

---
+++ TestGKNoise/TestGKNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGKNoiseApp.swift
+//  TestGKNoise
+//
+//  Created by Rick Mann on 2021-04-29.
+//
+
+import SwiftUI
+
+@main
+struct TestGKNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
